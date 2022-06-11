80f3d1f60cf81989c3ca42ae740c0a8d59b57efa
Steve Mortimer <steve.mortimer@lumen.com>
2022-06-10 21:10:38 -0500

    updated images and media

--- js/party.js
+++ js/party.js
@@ -2,9 +2,11 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
   get: (searchParams, prop) => searchParams.get(prop),
 });
 
-document.getElementById('name').innerText = params.name;
+let image = 'images/' + params.name + '.png';
+let audio = 'media/' + params.name + '.wav';
 
-const media = params.media;
-let welcomeAudio = document.getElementById('welcomeAudio');
-welcomeAudio.value = media;
-welcomeAudio.play();
+document.getElementById('personalized-image').src = image;
+
+document.getElementById('audioSource').src = audio;
+document.getElementById('audio').load();
+document.getElementById('audio').play();
